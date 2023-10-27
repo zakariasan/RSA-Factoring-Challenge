18f241fc360087145963e2a78b4bc84a430d5184
zakariasan <elitziko@gmail.com>
2023-10-27 14:27:18 +0100

    all in for rsa

--- rsa_factors.c
+++ rsa_factors.c
@@ -1,36 +1,7 @@
 #include "rsa_factors.h"
-/**
-* _atoi - Convert a string to an integer.
-* @s: String input.
-*
-* Description: function that convert a string to an integer.
-* Return: int value from String.
-*/
-int _atoi(char *s)
-{
-	int i;
-	size_t nbr;
-	int n;
+#include <stddef.h>
+#include <stdio.h>
 
-	nbr = 0;
-	i = 0;
-	n = 1;
-	while ((s[i] < '0' || s[i] > '9') && s[i])
-	{
-		if (s[i] == '-')
-			n *= -1;
-		i++;
-	}
-	if (s[i] >= '0' && s[i] <= '9' && s[i])
-	{
-		while (s[i] >= '0' && s[i] <= '9' && s[i])
-		{
-			nbr = nbr * 10 + s[i] - '0';
-			i++;
-		}
-	}
-	return (n * nbr);
-}
 /**
  * main - main Rsa Implementation in the code
  * @ac: arg nbrs
@@ -43,7 +14,7 @@ int main(int ac, char **av)
 	FILE *file;
 	char *line;
 	size_t size;
-	size_t  nbr, i;
+	size_t nbr, i;
 
 	if (ac != 2)
 	{
@@ -60,18 +31,21 @@ int main(int ac, char **av)
 
 	while (getline(&line, &size, file) != EOF)
 	{
-		nbr = atoi(line);
+		nbr = atoll(line);
 		i = 2;
 		while (i <= (nbr / 2))
 		{
 			if (nbr % i == 0)
 			{
 				printf("%ld=%ld*%ld\n", nbr, nbr / i, i);
-				i = nbr;
+				i = nbr + 2;
 			}
 			i++;
 		}
 	}
+	if(line)
+		free(line);
+	fclose(file);
 
 	return (EXIT_SUCCESS);
 }
